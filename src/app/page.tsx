fc86c13cfc22eb74c440edb23e0e2b5d74635cd5
SangHoon Lee <s20604@naver.com>
2023-11-23 23:40:00 +0900

    optimized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ import { useSpring, animated, easings, useInView } from "@react-spring/web";
 import { useState, useEffect, use, CSSProperties } from "react";
 import { useGesture } from "@use-gesture/react";
 
-export default function Home() {
+export default function Home()
+{
   // js
 
   const [spring, setSpring] = useSpring(() => ({
@@ -16,7 +17,8 @@ export default function Home() {
   }));
 
   const bind = useGesture({
-    onMove: ({ xy }) => {
+    onMove: ({ xy }) =>
+    {
       const [mx, my] = xy;
       const { width, height, top, left } =
         document.getElementById("logo")?.getBoundingClientRect() || {};
@@ -29,13 +31,14 @@ export default function Home() {
 
       setSpring({
         transform: `perspective(500px) rotateX(${angleX}deg) rotateY(${angleY}deg)`,
-        filter: `drop-shadow(${-x * 0.03}px ${
-          -y * 0.03
-        }px 0px rgba(0, 0, 0, 0.5))`,
+        filter: `drop-shadow(${-x * 0.03}px ${-y * 0.03
+          }px 0px rgba(0, 0, 0, 0.5))`,
       });
     },
-    onHover: ({ hovering }) => {
-      if (!hovering) {
+    onHover: ({ hovering }) =>
+    {
+      if (!hovering)
+      {
         setSpring({
           transform: "perspective(500px) rotateX(-5deg) rotateY(-20deg)",
           filter: "drop-shadow(10px 0px 0px rgba(0, 0, 0, 0.5))",
@@ -92,8 +95,10 @@ export default function Home() {
 
   const [doorfontSize, setdoorFontSize] = useState<number>(1000);
 
-  useEffect(() => {
-    const handleResize = () => {
+  useEffect(() =>
+  {
+    const handleResize = () =>
+    {
       const parent = document.querySelector(".main_door");
       const parentheight = parent?.clientHeight || 0;
 
@@ -103,7 +108,8 @@ export default function Home() {
     handleResize();
     window.addEventListener("resize", handleResize);
 
-    return () => {
+    return () =>
+    {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
@@ -114,8 +120,10 @@ export default function Home() {
 
   const [infofontSize, setinfoFontSize] = useState<number>(1000);
 
-  useEffect(() => {
-    const handleResize = () => {
+  useEffect(() =>
+  {
+    const handleResize = () =>
+    {
       const parent = document.querySelector(".main_info");
       const parentwidth = parent?.clientWidth || 0;
 
@@ -125,7 +133,8 @@ export default function Home() {
     handleResize();
     window.addEventListener("resize", handleResize);
 
-    return () => {
+    return () =>
+    {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
@@ -141,7 +150,7 @@ export default function Home() {
         <div className="main_door">
           <animated.div className="main_door_text" style={doorfontsize}>
             <animated.h1 className="door_h1" style={textfade}>
-              무언가 텍스트
+              Start of Change
             </animated.h1>
             <animated.h2 className="door_h2" style={textfade2}>
               위브먼트
@@ -166,16 +175,15 @@ export default function Home() {
             ref={inViewRef1}
           >
             <div className="info_content_box_text" style={infofontsize}>
-              <h1 className="info_content_h1">대충 무슨 어떠한 주제</h1>
+              <h1 className="info_content_h1">Aily (AI Recycler)</h1>
               <div className="info_content_text_separater"></div>
               <p className="info_content_p">
-                대충 이 주제에 대한 설명 대충 이 주제에 대한 설명 <br />
-                대충 이 주제에 대한 설명 대충 이 주제에 대한 설명{" "}
+                컴퓨터비전 AI를 활용한 자동 쓰레기분류장치 AIoT 앱/웹 <br />
+                &lt;2023 한국전자전 KES 출품&gt;{" "}
               </p>
             </div>
             <div className="info_content_box_image">
-              대충 이미지
-              <animated.img className="info_content_box_image_example" />
+              <animated.img src="images/AilyLogo.png" className="info_content_box_image_example" />
             </div>
           </animated.div>
           <animated.div
@@ -184,15 +192,18 @@ export default function Home() {
             ref={inViewRef2}
           >
             <div className="info_content_box_image">
-              대충 이미지
-              <animated.img className="info_content_box_image_example" />
+
+              <animated.img src="images/SolvemateLogo.png" className="info_content_box_image_example" />
+              {/* <a href='https://play.google.com/store/apps/details?id=com.wevement.solvemate&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                <img alt='다운로드하기 Google Play' style={{ width: `100%` }} src='https://play.google.com/intl/en_us/badges/static/images/badges/ko_badge_web_generic.png' />
+              </a> */}
             </div>
             <div className="info_content_box_text" style={infofontsize}>
-              <h1 className="info_content_h1">대충 무슨 어떠한 주제</h1>
+              <h1 className="info_content_h1">나만의 문제친구</h1>
               <div className="info_content_text_separater"></div>
               <p className="info_content_p">
-                대충 이 주제에 대한 설명 대충 이 주제에 대한 설명 <br />
-                대충 이 주제에 대한 설명 대충 이 주제에 대한 설명{" "}
+                ChatGPT를 활용한 <br />
+                교육 / 문제 솔루션{" "}
               </p>
             </div>
           </animated.div>
@@ -202,16 +213,15 @@ export default function Home() {
             ref={inViewRef3}
           >
             <div className="info_content_box_text" style={infofontsize}>
-              <h1 className="info_content_h1">대충 무슨 어떠한 주제</h1>
+              <h1 className="info_content_h1">CPR Kiosk</h1>
               <div className="info_content_text_separater"></div>
               <p className="info_content_p">
-                대충 이 주제에 대한 설명 대충 이 주제에 대한 설명 <br />
-                대충 이 주제에 대한 설명 대충 이 주제에 대한 설명{" "}
+                교육용 CPR 키오스크 앱 개발 <br />
+                [경상북도청 납품]{" "}
               </p>
             </div>
             <div className="info_content_box_image">
-              대충 이미지
-              <animated.img className="info_content_box_image_example" />
+              <animated.img src="images/CPRKiosk.png" className="info_content_box_image_example2" />
             </div>
           </animated.div>
         </div>
